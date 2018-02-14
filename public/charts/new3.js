45e3a850d38bd7ac4b004cd4f71a0c8d2cd9569e
Lee Norgaard <leemnorgaard@gmail.com>
2018-02-14 10:41:21 -0800

    added 3rd visualization with countries

--- public/charts/new3.js
+++ public/charts/new3.js
@@ -12,7 +12,8 @@ TODO: fix data so that start and end works for every json file, not just some
     and end time of the request.
 
 */
-
+titles = {"epochtime":0,"timestamp":1,"duration":2,"name":3,"name.previous":4,"country":5,"city":6}
+numRequests = 0
 
 // Settings for svg/d3 stuff
 var margin = $data.margin;
@@ -21,9 +22,8 @@ var height = 900 - margin.top - margin.bottom;
 var padding = 20;
 
 // Our data structures
-var allPages = []; 
-var nodesByPage = []; 
 var pageHierarchy = [];
+var nodesByCity = {};
 var stream;
 
 
@@ -36,60 +36,101 @@ function initTraffic(div) {
     drawCircles(div);
 }
 
+// Given a list of requests from the same city, add them to the hierarchy
+function addDataToHierarchy(dict) {
+    var name = dict["name"];
+    var children = dict["children"]; 
+    var data = nodesByCity[name];
+    for (var i = 0; i < data.length; i++) {
+        var d = data[i];
+        var newEntry = {};
+        for (t in titles) {
+            newEntry[t] = d[titles[t]];
+        }
+        newEntry["name"] = (newEntry["duration"]).toString();
+        children.push(newEntry);
+    }
+}
+
 
 /** Empties and repopulates nodesByPage dictionary 
         newData: all the data, formatted like the json file
         nodesByPage: dictionary of all the requests data with pages as keys
 */
 function formatData(newData) {
+    numRequests = newData.length
+    console.log("num requests = ", numRequests);
+
+    // Initialize the hierarchy
     pageHierarchy = [];
     pageHierarchy[0] = {};
     pageHierarchy[0]["name"] = "app";
     pageHierarchy[0]["children"] = [];
     var currentLevel = pageHierarchy[0]["children"];
 
+    // Create a list of all countries, create a list of all cities, and create a dictionary
+    // with cities as keys and countries as values
+    var allCountries = [];
+    var allCities = [];
+    var cityCountryDict = {};
+
     for (i = 0; i < newData.length; i++) {
-        var pageName = newData[i][dimensionIndex];
-        var notDone = true; 
-        var levelSize = currentLevel.length;  
-        var currIndex = 0;    
+        var city = newData[i][titles["city"]]
+        var country = newData[i][titles["country"]]
 
-        // Keep looping until we find the right place to but the URL
-        while (notDone && (currIndex < levelSize)) {
-            var currDict = currentLevel[currIndex];
+        // List of countries
+        if (allCountries.indexOf(country) <= -1) {
+            allCountries.push(country);
+        }
 
-            // Case 1: found it
-            if (currDict["name"] == pageName) { 
-                var children = currDict["children"]; 
-                var newEntry = {};
-                console.log("start: ", newData[i][2]);
-                newEntry["start"] = newData[i][2];
-                newEntry["end"] = newData[i][1];
-                newEntry["name"] = (newEntry["start"]).toString();
-                children.push(newEntry);
-                notDone = false;
-            } 
+        // List of cities
+        if (allCities.indexOf(city) <= -1) {
+            allCities.push(city);
+        }
 
-            // Case 2: didn't find it yet
-            else { 
-                currIndex = currIndex + 1;
+        // Dictionary (key=city, value=country)
+        var currentKeys = Object.keys(cityCountryDict)
+        if ((city in currentKeys) == false) {
+            cityCountryDict[city] = country
+        }
+
+        // Dictionary (key=city, value=list of requests from that city)
+        if (nodesByCity[city] == null) {
+            nodesByCity[city] = [];
+            nodesByCity[city].push(newData[i]);
+        } else {
+            nodesByCity[city].push(newData[i]);
+        }
+    }
+
+    console.log("countries: ", allCountries);
+    console.log("cities: ", allCities);
+    console.log("cityCountryDict: ", cityCountryDict);
+    console.log("nodesByCity: ", nodesByCity)
+
+    // Add the countries to the hierarchy
+    for (co = 0; co < allCountries.length; co++) {
+        currCountry = allCountries[co]
+        var newDict = {};
+        newDict["name"] = currCountry;
+        newDict["children"] = [];
+        innerLevel = newDict["children"]
+
+        // Add the cities to the hierarchy
+        for (ci = 0; ci < allCities.length; ci++) {
+            city = allCities[ci]
+            country = cityCountryDict[city]
+            if (country == currCountry) {
+                var newDict2 = {};
+                newDict2["name"] = city;
+                newDict2["children"] = [];
+                addDataToHierarchy(newDict2)
+                innerLevel.push(newDict2);
             }
         }
-        if (notDone) {
-
-            var newDict = {};
-            newDict["name"] = pageName;
-            newDict["children"] = [];
-
-            var newEntry = {};
-            newEntry["start"] = newData[i][2];
-            newEntry["end"] = newData[i][1];
-            newEntry["name"] = (newEntry["start"]).toString();
-            newDict["children"].push(newEntry);
-            currentLevel.push(newDict);
-        }
-    }   
-
+        currentLevel.push(newDict);
+    }
+    console.log("pageHierarchy: ", pageHierarchy);
 }
 
 
@@ -116,7 +157,7 @@ function drawCircles(div) {
         .padding(2);
 
     var root = d3.hierarchy(pageHierarchy[0])
-              .sum(function(d) { return ((d.end + d.start)/2.0); })
+              .sum(function(d) { return d.duration; })
               .sort(function(a, b) { return b.start - a.start; });
 
     var focus = root,
@@ -174,11 +215,9 @@ function drawCircles(div) {
 
 // This is our main function that calls all the helper functions
 function updateTraffic(error, json, div, dimensionIndex) {
-    console.log("here?");
     if (error) {
         readout(error);
     } else {
-      console.log("here?????");
         var now = new Date().getTime();
         div.node().stream = {dimension: dimensionIndex, start: now, wall: now, sleep: 0, first: json[0][0], last: json[json.length-1][0], index: 0, data: json};
         stream = div.node().stream;
